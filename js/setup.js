6985578fc788714b9d4682a4d41ce94b74d913b1
unknown <isthatpawz@gmail.com>
2020-06-20 15:27:03 +0300

    Правит ошибки по npm test

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var wizardSetupWindow = document.querySelector('.setup');
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('div');
 var similarWizardList = document.querySelector('.setup-similar-list');
@@ -12,29 +14,29 @@ var WIZARD_LASTNAME = ['да Марья', 'Верон', 'Мирабелла', '�
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYE_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var getRandom = function(elements) {
-  var randomElement = elements[Math.floor(Math.random()*elements.length)];
+var getRandom = function (elements) {
+  var randomElement = elements[Math.floor(Math.random() * elements.length)];
   return randomElement;
-}
+};
 
 for (var i = 0; i < 4; i++) {
   wizards[i] = {
     name: getRandom(WIZARD_NAME) + ' ' + getRandom(WIZARD_LASTNAME),
     coatColor: getRandom(COAT_COLOR),
     eyesColor: getRandom(EYE_COLOR)
-  }
+  };
 }
 
-var upgradeWizard = function(wizard) {
+var upgradeWizard = function (wizard) {
   var element = wizardTemplate.cloneNode(true);
   element.querySelector('.setup-similar-label').textContent = wizard.name;
   element.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   element.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return element;
-}
+};
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(upgradeWizard(wizards[i]));
+for (var j = 0; j < wizards.length; j++) {
+  fragment.appendChild(upgradeWizard(wizards[j]));
 }
 similarWizardList.appendChild(fragment);
